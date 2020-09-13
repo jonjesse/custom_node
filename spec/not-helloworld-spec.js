4dff5b731527a3b265cf6bdf06ebd45678cee35a
root <root@sys1.attlocal.net>
2020-09-12 21:20:22 -0500

    h

--- spec/not-helloworld-spec.js
+++ spec/not-helloworld-spec.js
@@ -6,7 +6,7 @@ describe("Hello World Server", function() {
   describe("GET /", function() {
     it("returns status code 200", function(done) {
       request.get(base_url, function(error, response, body) {
-        console.log(response);
+        //console.log(response);
 	var rcode = response.statusCode;
 	//console.log("status code::"+response.statusCode) 
         expect(rcode).toBe(200);
