484580b103761d81dae8c8461988065dca1a587f
jonjesse <root@sys1.attlocal.net>
2020-09-12 23:27:02 -0500

    done

--- app.js
+++ app.js
@@ -6,5 +6,7 @@ app.get('/', (req, res) => res.send('this is NOT hello world...'))
 var server = app.listen(3000, () => {
     console.log("Listening on port " + server.address().port + "...");
 });
-console.log('starting');
+setTimeout(function() {
+    console.log('sleeping 3s');
+}, 3000);
 module.exports = server;
