1cf615eaa5a1773744b29bcaa2f343bb6057b4ec
jonjesse <root@sys1.attlocal.net>
2020-09-12 21:26:31 -0500

    fixed

--- spec/not-helloworld-spec.js
+++ spec/not-helloworld-spec.js
@@ -1,13 +1,14 @@
 var request = require("request");
 var base_url = "http://localhost:3000/";
 var server = require("../app.js");
+var rcode = '';
 
 describe("Hello World Server", function() {
   describe("GET /", function() {
     it("returns status code 200", function(done) {
       request.get(base_url, function(error, response, body) {
         //console.log(response);
-	var rcode = response.statusCode;
+	rcode = response.statusCode;
 	console.log("status code::"+rcode); 
         expect(rcode).toBe(200);
         done();
