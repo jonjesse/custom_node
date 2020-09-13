ec7f5f062ff2d7366e1c98f21b7dc8e7880c4c5a
jonjesse <root@sys1.attlocal.net>
2020-09-13 14:24:19 -0500

    x

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var exp = require('express');
 var app = exp();
 
-app.get('/', (req, res) => res.send('this is NOT hello world...'));
+app.get('/', function(req, res) {
+  res.send('this is NOT hello world...');
+});
 var sleep = require('system-sleep');
 sleep(10);
 
-var server = app.listen(3000, () => {
+var server = app.listen(3000, function()  {
     console.log("Listening on port " + server.address().port + "...");
 });
 module.exports = server;
