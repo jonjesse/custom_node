27b268a96531c1cb4c01e106d404a4619abf8f6a
jonjesse <root@sys1.attlocal.net>
2020-09-13 12:58:34 -0500

    fix

--- spec/not-helloworld-spec.js
+++ spec/not-helloworld-spec.js
@@ -5,9 +5,11 @@ var rcode = '';
 
 describe("Hello World Server", function() {
   describe("GET /", function() {
+    var sleep = require('system-sleep');
+    sleep(100);
     it("returns status code 200", function(done) {
       request.get(base_url, function(error, response, body) {
-        console.log(response);
+        //console.log(response);
 	rcode = response;
 	console.log("status code::"+rcode.statusCode); 
         expect(rcode.statusCode).toBe(200);
